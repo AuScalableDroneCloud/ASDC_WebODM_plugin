c839a14d7304d18df307bf9b6e126b8ccaaf871c
Owen Kaluza <owen.kaluza@monash.edu>
2022-11-11 14:41:55 +1100

    Fix links
    These should probably just call pipeline_project and open with mounts
    to replace the files entry below...

--- plugin.py
+++ plugin.py
@@ -48,9 +48,10 @@ class Plugin(PluginBase):
         return [#Menu("ASDC", self.public_url(""), "fas fa-road"),
                 Menu("ASDC Tools", "#", "fas fa-tools", submenu=[
                     Menu("Settings", self.public_url(""), "fas fa-cog"),
-                    Menu("JupyterHub - base", f"https://jupyter.{host}/", "fab fa-python"),
-                    Menu("JupyterHub - gpu", f"https://jupyter.{host}/", "fab fa-python"),
-                    Menu("JupyterHub - ml", f"https://jupyter.{host}/", "fab fa-python"),
+                    Menu("JupyterHub", f"https://jupyter.{host}", "fab fa-python"),
+                    Menu("JupyterHub - base", f"https://jupyter.{host}/hub/spawn?profile=base", "fab fa-python"),
+                    Menu("JupyterHub - gpu", f"https://jupyter.{host}/hub/spawn?profile=gpu", "fab fa-python"),
+                    Menu("JupyterHub - ml", f"https://jupyter.{host}/hub/spawn?profile=ml", "fab fa-python"),
                     Menu("Project Files", f"javascript:pipeline_project('{user.email}', '{host}', 'base');", "fas fa-folder-open icon"),
                     #Menu("Cesium", self.public_url("cesium/"), "fas fa-globe-asia"),
                     #Menu("Terria", self.public_url("terria/"), "fas fa-map"),
