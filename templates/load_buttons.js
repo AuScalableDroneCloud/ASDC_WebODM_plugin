0d7996b7b90ff6eebbfce8e3aac020440cc2f587
Owen Kaluza <owen.kaluza@monash.edu>
2023-02-17 15:41:44 +1100

    Improvements to file browsing open links

--- templates/load_buttons.js
+++ templates/load_buttons.js
@@ -29,7 +29,7 @@ function file_browser(host, user) {
     alert('Please select project(s) to browse on right...')
     return;
   }
-  spawnurl = `https://jupyter.${host}/hub/spawn?profile=base&projects=${projects}`;
+  spawnurl = `https://jupyter.${host}/hub/spawn?profile=base&projects=${projects}&next=encodeURIComponent('/user-redirect/lab/tree/projects')`;
   window.open(spawnurl)
   /*
   //SEE: https://jupyterhub.readthedocs.io/en/0.8.1/_static/rest-api/index.html
